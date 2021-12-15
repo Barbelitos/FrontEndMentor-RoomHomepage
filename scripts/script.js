2f57fe018086a8f4d33198b3dc5eaccd7e70ddcf
David Fonseca <davidfonseca@barbelitos-mbp.lan>
2021-12-15 10:17:08 +0200

    Slider created for pictures and text.

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,54 @@ closeMenu.addEventListener("click", () => {
   menu.classList.remove("show-menu");
   overlay.style.display = "none";
 });
+
+/* --------- SLIDER ---------*/
+const slider = function () {
+  const slides = document.querySelectorAll(".slide");
+  const textSlides = document.querySelectorAll(".slide-text");
+  const btnRight = document.querySelector(".slide-btn--right");
+  const btnLeft = document.querySelector(".slide-btn--left");
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+  // const textMaxSlide = textSlides.length;
+
+  // Functions
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+
+    textSlides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    goToSlide(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+
+    goToSlide(curSlide);
+  };
+
+  goToSlide(0);
+
+  // Event Handlers
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+};
+
+slider();
